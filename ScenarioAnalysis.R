ff123fdd47fe6ad2bf88b2aeaac547ab76f1ce90
TimeWz667 <timewz667@gmail.com>
2020-02-17 20:52:10 +0000

    Add scenario analysis

---
+++ ScenarioAnalysis.R
@@ -0,0 +1,32 @@
+#' todo meta data
+#'
+#'
+#'
+#'
+
+rm(list = ls())
+
+
+##### Load requirements -----
+source("Source/statistics.R")
+source("Source/analyseScenarios.R")
+load(file = "Output/Fitted.rdata")
+
+
+max_iter <- 1000
+set.seed(1166)
+
+
+##### Simulate -----
+res_scenarios <- list()
+for (i in names(list_bass)) {
+  res_scenarios[[i]] <- simulate_scenarios(list_bass[[i]], 0, 30, max_iter = max_iter)
+  print(paste0(i, " -- completed"))
+}
+
+
+res_agg_scenarios <- aggregate_scenarios(res_scenarios)
+
+
+##### Output -----
+save(res_scenarios, res_agg_scenarios, file = "Output/Scenarios.rdata")
---
+++ Source/summarise.R
@@ -0,0 +1,41 @@
+summary_fitted <- function(lbass, lbf) {
+
+  res <- rbindlist(lapply(lbass, function(f) {
+    with(f$Parameters, {
+      n_t <- nrow(f$Cases_hat)
+      cases <- f$Cases_hat[nrow(f$Cases_hat),]
+      sus <- m - cases - f$Cases$A[n_t] 
+      
+      r0 <- beta / sum(f$Offsets)
+      re <- r0 * sus / m
+      
+      list(
+        Location = f$Summary$Location,
+        "Kappa * 100" = stats_fn(kappa * 100), 
+        Beta = stats_fn(beta), 
+        "Population at risk (thousand)" = stats_fn(m / 1E3), 
+        R0 = stats_fn(r0),
+        "R(t)" = stats_fn(re),
+        Deviance = stats_fn(deviance)
+      )
+    })
+  }))
+  
+  res <- res[order(Location), ]
+  
+  bfs <- rbindlist(lbf)
+  bfs$BF <- round(1 / bfs$BF.SIR, 2)
+  bfs$DIC.BassSIR <- round(bfs$DIC.BassSIR, 2)
+  bfs$DIC.SIR <- round(bfs$DIC.SIR, 2)
+  bfs <- bfs[, c("Location", "BF", "DIC.BassSIR", "DIC.SIR")]
+
+  
+  res <- merge(res, bfs, by = "Location")
+  
+  return(res)
+}
+
+
+summary_intervention <- function() {
+  
+}

edccc1a5f7cf0a20f45dbb27346ed891c19ccebc
TimeWz667 <timewz667@gmail.com>
2020-02-29 23:55:35 +0000

    Run scenario analysis

--- ScenarioAnalysis.R
+++ ScenarioAnalysis.R
@@ -10,27 +10,57 @@
 
 rm(list = ls())
 
+options(odin.verbose = F,
+        odin.target = ifelse(odin::can_compile(), "c", "r"),
+        odin.compiler_warnings = F,
+        odin.no_check_unused_equations = T
+)
+
 
 ##### Load requirements -----
-source("Source/statistics.R")
-source("Source/analyseScenarios.R")
-load(file = "Output/Fitted.rdata")
+library(BassSIR)
+source("Source/scenarios.R")
+load(file = "Output/Simulation.rdata")
 
 
-max_iter <- 1000
-set.seed(1166)
+##### Lockdown intervention -----
+intv_lockdown <- list()
 
-
-##### Simulate -----
-res_scenarios <- list()
-for (i in names(list_bass)) {
-  res_scenarios[[i]] <- simulate_scenarios(list_bass[[i]], 0, 30, max_iter = max_iter)
-  print(paste0(i, " -- completed"))
+for (pro in names(sims_bass)) {
+  sim <- sims_bass[[pro]]
+  lockdown <- run_scenario(sim, zero_kappa)
+  intv_lockdown[[pro]] <- compare_scenarios(sim, Lockdown = lockdown, fn_change = "Y0")
 }
 
 
-res_agg_scenarios <- aggregate_scenarios(res_scenarios)
+##### Scenarios -----
+res_scenarios <- list()
 
+s1 <- gen_rec_decay(0.2)
+s2 <- gen_rec_decay(0.5)
+s3 <- gen_bost_m(at = 12, by = 2)
+s4 <- gen_bost_m(at = 19, by = 2)
+
+
+for (pro in names(sims_bass)) {
+  sim <- sims_bass[[pro]]
+
+  res_scenarios[[pro]] <- 
+    compare_scenarios(sim, 
+                      Rec2 = run_scenario(sim, s1),
+                      Rec5 = run_scenario(sim, s2),
+                      Open12 = run_scenario(sim, s3),
+                      Open19 = run_scenario(sim, s4),
+                      fn_change = "Yt")
+}
+
+scs <- c("Baseline", 
+         "Recovery rate decaying by 20% per month", 
+         "Recovery rate decaying by 50% per month", 
+         "Effective population size doubled after 24th Feb", 
+         "Effective population size doubled after 2nd Mar"
+)
 
 ##### Output -----
-save(res_scenarios, res_agg_scenarios, file = "Output/Scenarios.rdata")
+save(intv_lockdown, file = "Output/Intervention.rdata")
+save(res_scenarios, scs, file = "Output/Scenarios.rdata")
